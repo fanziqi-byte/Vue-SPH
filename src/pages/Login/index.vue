49fae40f3f329cc7e0fc8183e346efd6bb461773
FanZiQi <fanziqi_code@163.com>
2022-04-03 21:10:29 +0800

    2022/4/3/01

---
+++ src/pages/Login/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Login组件
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Search/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Search组件
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
